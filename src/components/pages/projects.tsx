862ea13d08e878f3b366349d9529a7594bde7958
Jiangshuuu <globelex65@gmail.com>
2024-09-01 13:48:17 +0800

    upd mobile

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -220,8 +220,8 @@ const Work = () => {
               })}
               {/* slider buttons */}
               <WorkSliderBtns
-                containerStyles="flex  gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                btnStyles="bg-accent rounded-full cursor-pointer hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="xl:bg-accent bg-white/30 xl:rounded-full cursor-pointer hover:bg-accent-hover text-gray-600 xl:text-primary text-[22px] w-[30px] xl:w-[44px] h-[44px] flex justify-center items-center transition-all"
               />
             </Swiper>
           </div>
